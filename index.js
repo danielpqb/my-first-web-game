1cf9ce57812ba9fc022297555d0ad9f43bea9740
Daniel <danielpqb@gmail.com>
2022-06-12 11:00:08 -0300

    Almost implementing collisions

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 class Player {
     constructor({
         image = playerImage,
-        position = { x: -1100, y: -1000 },
+        position = { x: 0, y: 0 }, //Pixel
         velocity = 1,
         direction = 0,
         isMirrored = false,
@@ -68,12 +68,12 @@ class Player {
             this.image,
             this.image.width / 6 * (this.frameColumn - 1), //Image Crop Xo
             this.image.height / 4 * (this.frameRow - 1), //Image Crop Yo
-            this.image.width / 6, //Image Crop Xo Offset
-            this.image.height / 4,//Image Crop Yo Offset
+            this.image.width / 6, //Image Crop X Offset
+            this.image.height / 4,//Image Crop Y Offset
             canvas.width / 2 - this.image.width / 12, //Image Xo Position on Canvas
             canvas.height / 2 - this.image.height / 8,//Image Yo Position on Canvas
-            this.image.width / 6, //Image Xo Position Offset on Canvas
-            this.image.height / 4 //Image Yo Position Offset on Canvas
+            this.image.width / 6, //Image X Position Offset on Canvas
+            this.image.height / 4 //Image Y Position Offset on Canvas
         )
         c.restore(); // Restore the state as it was when this function was called
     }
@@ -208,12 +208,14 @@ class Player {
 class Background {
     constructor({
         image = backgroundImage,
+        position = { x: (canvas.width / 2 - 24), y: (canvas.height / 2 - 48) } //Position of background starts at (x: 0, y: 0) relative to player
     }) {
         this.image = image
+        this.position = position
     }
 
     draw(x, y) {
-        c.drawImage(this.image, x, y)
+        c.drawImage(this.image, x + this.position.x, y + this.position.y)
     }
 }
 
@@ -256,6 +258,21 @@ class Controller {
     }
 }
 
+class Boundary {
+    static width = 48
+    static height = 48
+    constructor({ position }) {
+        this.position = position
+        this.width = 48
+        this.height = 48
+    }
+
+    draw(x, y) { //x and y are the offset coordinate of player position
+        c.fillStyle = 'rgba(255, 0, 0, 0.5)'
+        c.fillRect(this.position.x + x, this.position.y + y, this.width, this.height)
+    }
+}
+
 //Do something every frame
 function animate() {
 
@@ -285,11 +302,25 @@ function animate() {
     }
 
     background.draw(player.position.x, player.position.y) //Draw background
+    // collisionTiles.forEach(block => {
+    //     block.draw(player.position.x, player.position.y) //Draw collisionTiles
+    // })
+    const collisionTest = new Boundary({
+        position: {
+            x: 0,
+            y: 0
+        }
+    })
+    collisionTest.draw(background.position.x + player.position.x, background.position.y + player.position.y)
+
     player.draw() //Draw player
 
     player.move() //Move player
 
-
+    if (controller.t % 120 == 0) {
+        console.log('player: ' + player.position.x + ', ' + player.position.y)
+        console.log('block: ' + collisionTest.position.x + ', ' + collisionTest.position.y)
+    }
 
 
     controller.t %= 216000 //Reset counter every hour
@@ -322,6 +353,41 @@ const background = new Background({})
 const player = new Player({})
 const controller = new Controller({})
 
+const collisionsMap = []
+for (let i = 0; i < collisions.length; i += 110) {
+    collisionsMap.push(collisions.slice(i, 110 + i))
+}
+
+/*
+row: Array of tiles on 'y' position
+id: ID of the tile on 'x' position
+x: X coordinate of the tile
+y: Y coordinate of the tile */
+const collisionTiles = []
+collisionsMap.forEach((row, y) => {
+    row.forEach((id, x) => {
+        if (id === 3251)
+            collisionTiles.push(new Boundary({
+                position: {
+                    x: x * Boundary.width,
+                    y: y * Boundary.height
+                }
+            }))
+    })
+})
+/*  Example (10 x 10 Map):    x:0  1  2  3  4  5  6  7  8  9  y:
+    0 1 1 1 1 1 1 1 1 1   row: [0, 1, 1, 1, 1, 1, 1, 1, 1, 1] 0  => id: 0, id: 1, id: 1 ....
+    0 0 1 1 1 1 1 1 1 1   row: [0, 0, 1, 1, 1, 1, 1, 1, 1, 1] 1
+    0 0 0 1 1 1 1 1 1 1   row: [0, 0, 0, 1, 1, 1, 1, 1, 1, 1] 2
+    0 0 0 0 1 1 1 1 1 1   row: [0, 0, 0, 0, 1, 1, 1, 1, 1, 1] 3
+    0 0 0 0 0 1 1 1 1 1   row: [0, 0, 0, 0, 0, 1, 1, 1, 1, 1] 4
+    0 0 0 0 0 1 0 0 0 0   row: [0, 0, 0, 0, 0, 1, 0, 0, 0, 0] 5
+    0 0 0 0 1 0 0 0 0 0   row: [0, 0, 0, 0, 1, 0, 0, 0, 0, 0] 6
+    0 0 0 1 0 0 0 0 0 0   row: [0, 0, 0, 1, 0, 0, 0, 0, 0, 0] 7
+    0 0 1 0 0 0 0 0 0 0   row: [0, 0, 1, 0, 0, 0, 0, 0, 0, 0] 8
+    0 1 0 0 0 0 0 0 0 0   row: [0, 1, 0, 0, 0, 0, 0, 0, 0, 0] 9 */
+
 controller.listenKeysDown()
 animate()
-    
+
+
