cb716039457717d9644f41e0fa89bd7091cdb610
Daniel <danielpqb@gmail.com>
2022-06-13 19:58:03 -0300

    Monsters are moving and colliding!!!

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 class Player {
     constructor({
-        position = { x: (canvas.width / 2 - 24), y: (canvas.height / 2 - 48) },
         image = playerImage,
         velocity = 1,
         direction = 0,
@@ -13,7 +12,7 @@ class Player {
         isMoving = false
     }) {
         this.image = image
-        this.position = position
+        this.position = { x: (canvas.width / 2 - 24), y: (canvas.height / 2 - 48) } //Offset player position to be relative to background
         this.velocity = velocity
         this.direction = direction
         this.isMirrored = isMirrored
@@ -256,6 +255,207 @@ class Player {
     }
 }
 
+class Monster {
+    constructor({
+        position = { x: 0, y: 0 }, //Relative to background
+        image = slimeImage,
+        velocity = 1,
+        direction = 0,
+        isMirrored = false,
+        frameRow = 1,
+        frameColumn = 1,
+        maxFrames = 4,
+        state = 'standing',
+        snared = false,
+        isMoving = false
+    }) {
+        this.position = position
+        this.image = image
+        this.velocity = velocity
+        this.direction = direction
+        this.isMirrored = isMirrored
+        this.frameRow = frameRow
+        this.frameColumn = frameColumn
+        this.maxFrames = maxFrames
+        this.state = state
+        this.snared = snared
+        this.isMoving = isMoving
+    }
+
+    changeState(state) {
+        if (this.state != state) {
+            this.frameColumn = 1
+            this.state = state
+        }
+
+        switch (state) {
+            case 'standing':
+                this.frameRow = 1
+                this.maxFrames = 4
+                break;
+
+            case 'walking':
+                this.frameRow = 2
+                this.maxFrames = 6
+                break;
+
+            case 'attacking':
+                this.frameRow = 4
+                this.maxFrames = 3
+                break;
+
+            case 'dead':
+                this.frameRow = 5
+                this.maxFrames = 5
+                break;
+
+            default:
+                this.frameRow = 1
+                this.maxFrames = 4
+                break;
+        }
+    }
+
+    draw() {
+        c.save();  // Save the current canvas state
+        if (this.isMirrored) {
+            c.setTransform(-1, 0, 0, 1, canvas.width, 0); //Invert image horizontaly to the left
+        }
+        c.drawImage(
+            this.image,
+            this.image.width / 7 * (this.frameColumn - 1), //Image Crop Xo
+            this.image.height / 5 * (this.frameRow - 1), //Image Crop Yo
+            this.image.width / 7, //Image Crop X Offset
+            this.image.height / 5, //Image Crop Y Offset
+            controller.position.x + this.position.x, //Image Xo Position on Canvas
+            controller.position.y + this.position.y, //Image Yo Position on Canvas
+            this.image.width / 7, //Image X Position Offset on Canvas
+            this.image.height / 5 //Image Y Position Offset on Canvas
+        )
+        c.restore(); // Restore the state as it was when this function was called
+    }
+
+    move() {
+        //If monster is impeded to move, stop here
+        if (this.snared === true || this.state === 'attacking') {
+            this.isMoving = false
+            return
+        }
+
+        //Monster can't move for 5 seconds every 10 seconds
+        if (controller.t % 600 < 0) {
+            this.isMoving = false
+            return
+        }
+
+        //Wich direction should the monster go? (1 to 8)
+        if (controller.t % 300 == 0) { //Change direction every 5 seconds
+            let y = 0;
+            let x = 0;
+            const rndY = Math.floor(Math.random() * 2) //Up or Down? 0:Up | 1:Down
+            const rndX = Math.floor(Math.random() * 2) //Right or Left? 0:Right | 1:Left
+            const rndXY = Math.floor(Math.random() * 3) //Should it move on both directions? 0:Only Y | 1:Only X | 2:Both
+
+            if (rndY === 0) {
+                y = 1 //Up
+            } else {
+                y = 5 //Down
+            }
+            if (rndX === 0) {
+                x = 3 //Right
+                //this.isMirrored = false //Points Right
+            } else {
+                x = 7 //Left
+                //this.isMirrored = true //Points Left
+            }
+
+            switch (rndXY) {
+                case 0: //Only Y
+                    this.direction = y
+                    break;
+                case 1: //Only X
+                    this.direction = x
+                    break;
+                case 2: //Both Axis
+                    if (y === 1 && x === 7) { //Up+Left
+                        this.direction = 8 //Up+Left
+                    }
+                    else {
+                        this.direction = (x + y) / 2 //Up+Right || Down+Right || Down+Left
+                    }
+                    break;
+            }
+        }
+
+        //Save position to rollback if collided
+        const rollbackPosition = { x: this.position.x, y: this.position.y }
+
+        //Change monster position if it's trying to move (monster starts to move)
+        this.isMoving = true
+        this.changeState('walking')
+        switch (this.direction) {
+            case 1: //Up
+                this.position.y -= 1 * this.velocity
+                break;
+            case 2: //Up+Right
+                this.position.y -= 1 * this.velocity /// Math.sqrt(2)
+                this.position.x += 1 * this.velocity /// Math.sqrt(2)
+                break;
+            case 3: //Right
+                this.position.x += 1 * this.velocity
+                break;
+            case 4: //Down+Right
+                this.position.y += 1 * this.velocity /// Math.sqrt(2)
+                this.position.x += 1 * this.velocity /// Math.sqrt(2)
+                break;
+            case 5: //Down
+                this.position.y += 1 * this.velocity
+                break;
+            case 6: //Down+Left
+                this.position.y += 1 * this.velocity /// Math.sqrt(2)
+                this.position.x -= 1 * this.velocity /// Math.sqrt(2)
+                break;
+            case 7: //Left
+                this.position.x -= 1 * this.velocity
+                break;
+            case 8: //Up+left
+                this.position.y -= 1 * this.velocity /// Math.sqrt(2)
+                this.position.x -= 1 * this.velocity /// Math.sqrt(2)
+                break;
+            default:
+                return
+        }
+
+        //Check collisions
+        //const collidedBlocksDistances = [[], []] //Storage arrays of distance from monster to collided blocks
+        collisionTiles.forEach(block => {
+            const cRightDistance = ((this.position.x + 80) - (block.position.x)) //Right Distance
+            const cLeftDistance = ((this.position.x + 20) - (block.position.x + Boundary.width)) //Left Distance
+            const cUpDistance = ((this.position.y + 40) - (block.position.y + Boundary.height)) //Up Distance
+            const cDownDistance = ((this.position.y + 70) - (block.position.y)) //Down Distance
+            if (cRightDistance >= 0 && cLeftDistance <= 0 && cUpDistance <= 0 && cDownDistance >= 0) {
+                switch (this.direction) {
+                    case 1: //Up
+                    case 5: //Down
+                        this.position.y = rollbackPosition.y
+                        return;
+                    case 3: //Right
+                    case 7: //Left
+                        this.position.x = rollbackPosition.x
+                        return;
+                    default:
+                        this.position.x = rollbackPosition.x
+                        this.position.y = rollbackPosition.y
+                        return;
+                        //Pushes distance values to it's respective array
+                        collidedBlocksDistances[0].push(cRightDistance)
+                        collidedBlocksDistances[1].push(cUpDistance)
+                }
+            }
+        })
+    }
+}
+
 class Background {
     constructor({
         image = backgroundImage
@@ -263,15 +463,15 @@ class Background {
         this.image = image
     }
 
-    draw(x = 0, y = 0) {
-        c.drawImage(this.image, x + controller.position.x, y + controller.position.y)
+    draw() {
+        c.drawImage(this.image, controller.position.x, controller.position.y)
     }
 }
 
 class Controller {
     constructor({
         t = 0, //Count number of frames since animate starts
-        position = { x: -500, y: -900 },
+        position = { x: 0, y: 0 }, //Relative to canvas window
         lastKeyWS = '', //Was 'w' or 's' the Last Key Pressed
         lastKeyAD = '', //Was 'a' or 'd' the Last Key Pressed
         moveKeysDown = { w: isKeyDown('w'), a: isKeyDown('a'), s: isKeyDown('s'), d: isKeyDown('d') },
@@ -318,9 +518,9 @@ class Boundary {
         this.height = 48
     }
 
-    draw(x = 0, y = 0) { //x and y are the offset coordinate of player position
+    draw() { //x and y are the offset coordinate of player position
         c.fillStyle = 'rgba(255, 0, 0, 0.5)'
-        c.fillRect(this.position.x + controller.position.x + x, this.position.y + controller.position.y + y, this.width, this.height)
+        c.fillRect(this.position.x + controller.position.x, this.position.y + controller.position.y, this.width, this.height)
     }
 }
 
@@ -350,12 +550,39 @@ function animate() {
                 player.frameColumn = (player.frameColumn % player.maxFrames) + 1
                 break;
         }
+        trainingMapMonsters.forEach((monster) => {
+            switch (monster.state) {
+                case 'attacking':
+                    if (monster.frameColumn < monster.maxFrames) {
+                        monster.frameColumn++
+                    }
+                    else {
+                        monster.changeState('standing')
+                    }
+                    break;
+                case 'walking':
+                    if (monster.isMoving === true) {
+                        monster.frameColumn = (monster.frameColumn % monster.maxFrames) + 1
+                    }
+                    else {
+                        monster.changeState('standing')
+                    }
+                    break;
+                default:
+                    monster.frameColumn = (monster.frameColumn % monster.maxFrames) + 1
+                    break;
+            }
+        })
     }
 
     background.draw() //Draw background
+    //collisionTiles.forEach(block => { block.draw() })//Draw collisionTiles
+    trainingMapMonsters.forEach((monster) => {
+        monster.draw() //Draw monster
+        monster.move()
+    })
 
     player.draw() //Draw player
-
     player.move() //Move player
 
 
@@ -375,55 +602,64 @@ const isKeyDown = (() => {
     return (key) => state.hasOwnProperty(key) && state[key] || false;
 })();
 
+/* ------------------- */
+/* Program starts here */
+/* ------------------- */
+//HTML DOM object with a <canvas> tag
 const canvas = document.querySelector('canvas')
+//Window where the game will be shown
 const c = canvas.getContext('2d')
-canvas.width = 1536
-canvas.height = 864
+canvas.width = 1536 //Window width
+canvas.height = 864 //Window height
 
+//Create images as HTML DOM objects and refer there path
 const backgroundImage = new Image()
 backgroundImage.src = './img/trainingMap.png'
 const playerImage = new Image()
 playerImage.src = './img/player.png'
+const slimeImage = new Image()
+slimeImage.src = './img/slime.png'
 
+//Instanciate Classes
+const controller = new Controller({ position: { x: -800, y: -800 } }) //Relative to Canvas
 const background = new Background({})
 const player = new Player({})
-const controller = new Controller({})
 
+// Get collisions data and slice it.
+// Each array inside collisionsMap will have a size of map width (in tiles).
+// The number of arrays inside collisionsMap represents the number of tiles in Y coordinate.
+// The values inside arrays of collisionsMap represents the Id that 'Tiled' signed for each tile type.
 const collisionsMap = []
 for (let i = 0; i < collisions.length; i += 110) {
     collisionsMap.push(collisions.slice(i, 110 + i))
 }
-
-/*
-row: Array of tiles on 'y' position
-id: ID of the tile on 'x' position
-x: X coordinate of the tile
-y: Y coordinate of the tile */
+// row: Array of tiles on 'y' position
+// id: ID of the tile on 'x' position
+//     Example (10 x 3 Map):    x: 0  1  2  3  4  5  6  7  8  9  y:
+//     0 1 1 1 1 1 1 1 1 1   row: [0, 1, 1, 1, 1, 1, 1, 1, 1, 1] 0  => id: 0, id: 1, id: 1 ....
+//     0 0 1 1 1 1 1 1 1 1   row: [0, 0, 1, 1, 1, 1, 1, 1, 1, 1] 1
+//     0 0 0 1 1 1 1 1 1 1   row: [0, 0, 0, 1, 1, 1, 1, 1, 1, 1] 2
 const collisionTiles = []
 collisionsMap.forEach((row, y) => {
     row.forEach((id, x) => {
         if (id === 3251)
             collisionTiles.push(new Boundary({
                 position: {
-                    x: x * Boundary.width,
-                    y: y * Boundary.height
+                    x: x * Boundary.width, // X coordinate in pixels relative to Background
+                    y: y * Boundary.height // Y coordinate in pixels relative to Background
                 }
             }))
     })
 })
-/*  Example (10 x 10 Map):    x:0  1  2  3  4  5  6  7  8  9  y:
-    0 1 1 1 1 1 1 1 1 1   row: [0, 1, 1, 1, 1, 1, 1, 1, 1, 1] 0  => id: 0, id: 1, id: 1 ....
-    0 0 1 1 1 1 1 1 1 1   row: [0, 0, 1, 1, 1, 1, 1, 1, 1, 1] 1
-    0 0 0 1 1 1 1 1 1 1   row: [0, 0, 0, 1, 1, 1, 1, 1, 1, 1] 2
-    0 0 0 0 1 1 1 1 1 1   row: [0, 0, 0, 0, 1, 1, 1, 1, 1, 1] 3
-    0 0 0 0 0 1 1 1 1 1   row: [0, 0, 0, 0, 0, 1, 1, 1, 1, 1] 4
-    0 0 0 0 0 1 0 0 0 0   row: [0, 0, 0, 0, 0, 1, 0, 0, 0, 0] 5
-    0 0 0 0 1 0 0 0 0 0   row: [0, 0, 0, 0, 1, 0, 0, 0, 0, 0] 6
-    0 0 0 1 0 0 0 0 0 0   row: [0, 0, 0, 1, 0, 0, 0, 0, 0, 0] 7
-    0 0 1 0 0 0 0 0 0 0   row: [0, 0, 1, 0, 0, 0, 0, 0, 0, 0] 8
-    0 1 0 0 0 0 0 0 0 0   row: [0, 1, 0, 0, 0, 0, 0, 0, 0, 0] 9 */
 
+//Store monsters related to each map
+const trainingMapMonsters = []
+for (let i = 0; i < 20; i++) {
+    trainingMapMonsters.push(new Monster({ position: { x: 1200, y: 980 } }));
+}
+
+//Listen to keys pressed down
 controller.listenKeysDown()
+
+//Animate game every frame (Infinite loop that is executed 60 times per second)
 animate()
-
-
